b61777e6654de6a8340081f7d893be0972dc48e3
ninja-inc <takiida0605@gmail.com>
2017-02-19 16:48:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sigure-task
---
+++ src/main/java/koikijs/SigureTaskApplication.java
@@ -0,0 +1,12 @@
+package koikijs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigureTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigureTaskApplication.class, args);
+	}
+}
